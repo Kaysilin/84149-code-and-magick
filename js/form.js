89572d1ac86e8da606a3210c5ece71ff8b93bcd6
Kaysilin <kaysilin-git@yandex.ru>
2015-09-20 21:59:58 +0400

    Form validation and cookies without expiry date

--- js/form.js
+++ js/form.js
@@ -1,7 +1,17 @@
+"use strict";
 (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formElement = document.forms['review-form'];
+  var formReviewMark = document.querySelectorAll('.review-form-group-mark input');
+  var formReviewName = document.querySelector('#review-name');
+  var formReviewText = document.querySelector('#review-text');
+  var formReviewFields = document.querySelector('.review-fields');
+  var formReviewFieldsName = document.querySelector('.review-fields-name');
+  var formReviewFieldsText = document.querySelector('.review-fields-text');
+  formReviewFieldsName.isVisible = 0;
+  formReviewFieldsText.isVisible = 0;
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -12,4 +22,54 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  var validateForm = function(formReviewInput, formReviewInputLabel) {
+    if (formReviewInput.value) {
+      formReviewInputLabel.classList.add('invisible');
+      formReviewInputLabel.isVisible = 1;
+      if (formReviewFieldsName.isVisible && formReviewFieldsText.isVisible) {
+        formReviewFields.classList.add('invisible');
+      }
+    }
+    else
+    {
+      formReviewInputLabel.classList.remove('invisible');
+      formReviewInputLabel.isVisible = 0;
+      formReviewFields.classList.remove('invisible');
+    }
+  };
+
+  formReviewName.oninput = function(evt) {
+    evt.preventDefault();
+    validateForm(formReviewName, formReviewFieldsName);
+  };
+
+  formReviewText.oninput = function(evt) {
+    evt.preventDefault();
+    validateForm(formReviewText, formReviewFieldsText);
+  };
+
+  formElement.onsubmit = function(evt) {
+    evt.preventDefault();
+    console.log(formReviewMark);
+    for (var i = 0; i < formReviewMark.length; i++) {
+      // Достаточно ли будет проверки на наличие свойства с учетом того, что атрибут булевый или нужно еще проверять на значение свойства равное true?
+      if (formReviewMark[i].checked) {
+        docCookies.removeItem('review-mark');
+        docCookies.setItem('review-mark',formReviewMark[i].value);
+        console.log('review-mark' + formReviewMark[i].value);
+      }
+    }
+    docCookies.removeItem('review-name');
+    docCookies.setItem('review-name', formReviewName.value);
+    console.log('review-name', formReviewName.value);
+    if (formReviewFieldsName.isVisible && formReviewFieldsText.isVisible)   {
+      formElement.submit();
+    }
+    else
+    {
+      alert('Не все поля формы заполнены');
+    }
+  };
+
 })();
