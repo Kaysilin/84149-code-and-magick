d6a8cad1bd441f007d975a94ebd80823f39c6483
Kaysilin <kaysilin-git@yandex.ru>
2015-10-18 09:26:10 +0400

    Gallery  final

--- js/game_demo.js
+++ js/game_demo.js
@@ -12,25 +12,25 @@
   }
 
   function initScroll() {
+    var timeoutDisappear;
     window.addEventListener('scroll', function() {
       var TIMEOUT = 100;
-      var timeoutDisappear;
 
       doParallax(parallaxActive);
       clearTimeout(timeoutDisappear);
-      timeoutDisappear = setTimeout(window.dispatchEvent(new CustomEvent('cloudsdisappear')), TIMEOUT);
+      timeoutDisappear = setTimeout(doCloudDisappear, TIMEOUT);
 
     });
 
-    /* не совсем поняла задачу:
-    Оптимизируйте обработчик события scroll с помощью таймаута, который срабатывает каждые 100 миллисекунд и испускает кастомное событие «исчезновения блока с облаками» из поля зрения. NB! Смещение для параллакса должно пересчитываться не каждые 100 миллисекунд, а на каждое изменение скролла, оптимизация касается только проверки видимости блока с облаками.
-
-      Добавьте обработчик события, отключающий параллакс, реагирующий на событие «исчезновения блока с облаками» из поля зрения.
-
-      По факту если обработчик события будет только отключать параллакс, то нужно либо событие, которое будет его включать, либо то же самое переключение флага активности эффекта параллакса сделать в другом месте. Тогда не вижу смысла вообще в этом событии. Сделала так, чтобы пр испукании события его обработчик уже переключает флаг. Используются ли так события или все-таки обычно они используются для реакции на какое-то одно остояние? */
-    window.addEventListener('cloudsdisappear', function() {
+    function doCloudDisappear() {
       var elementCloudsPosition = elementClouds.getBoundingClientRect();
-      parallaxActive = elementCloudsPosition.bottom <= 0 ? 0 : 1;
+      if (elementCloudsPosition.bottom <= 0) {
+        window.dispatchEvent(new CustomEvent('cloudsdisappear'));
+      }
+    }
+
+    window.addEventListener('cloudsdisappear', function() {
+      parallaxActive = 0;
     });
   }
 
